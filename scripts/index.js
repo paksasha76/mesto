708eb3cf202b1b53426ac1f1b041f7f4949a4dee
paksasha76 <pak.aleksandr2001@mail.ru>
2023-02-08 17:41:40 +0300

    project

--- scripts/index.js
+++ scripts/index.js
@@ -7,8 +7,48 @@ let jobInput = document.querySelector(".popup__input_type_job");
 let profileName = document.querySelector(".profile__name");
 let profileJob = document.querySelector(".profile__job");
 
+let buttonAdd = document.querySelector(".profile__add-btn");
+let popupAdd = document.querySelector(".popup__add");
+let popupFormAdd =  document.querySelector(".popup__form-add");
+let nameInputCard = document.querySelector(".popup__input_type_card-name");
+let linkInputCard = document.querySelector(".popup__input_type_card-link");
+let popupEdit = document.querySelector(".popup__edit");
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+function openPopupAdd() {
+    popupAdd.classList.add("popup_opened");
+    nameInputCard.value = '';
+    linkInputCard.value = '';
+}
+
 function openPopup() {
-    popup.classList.add("popup_opened");
+    popupEdit.classList.add("popup_opened");
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 }
@@ -16,6 +56,7 @@ function openPopup() {
 function closePopup() {
     popup.classList.remove("popup_opened");
 }
+
 
 function handleFormSubmit(event) {
     event.preventDefault();
@@ -27,3 +68,5 @@ function handleFormSubmit(event) {
 buttonEdit.addEventListener("click", openPopup);
 buttonClose.addEventListener("click", closePopup);
 popupForm.addEventListener("submit", handleFormSubmit);
+popupFormAdd.addEventListener("submit", handleFormSubmit);
+buttonAdd.addEventListener("click", openPopupAdd);
